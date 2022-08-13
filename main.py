d5d59137e9ca1e243e2c777991b07730a6559ddd
sm.yoo <sm.yoo@testworks.co.kr>
2022-08-13 18:24:39 +0900

    feat: ini파일에 로그인 정보 저장 가능하게

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import click
 from browser.chrome import Chrome
 from logger.log import Log
+from configparser import ConfigParser
+import os
+
+
+def config(file_path: str = 'hiworks.ini'):
+    current_path = current_path = os.path.dirname(os.path.abspath(__file__))
+    config_parser = ConfigParser()
+    config_parser.read(os.path.join(current_path, file_path))
+    return config_parser
 
 
 @click.group()
@@ -9,17 +18,27 @@ def cli():
 
 
 @cli.command()
-@click.option('--id', required=True, type=click.types.STRING, help='hiworks id')
-@click.option('--passwd', required=True, type=click.types.STRING, help='hiworks password')
+@click.option('--id', required=False, type=click.types.STRING, help='hiworks id')
+@click.option('--passwd', required=False, type=click.types.STRING, help='hiworks password')
 def checkin(_id: str, passwd: str):
+    if _id is None or passwd is None:
+        conf = config()
+        _id = conf['default']['id']
+        passwd = conf['default']['password']
+
     browser = Chrome(Log.logger('checkin'))
     browser.checkin(_id, passwd)
 
 
 @cli.command()
-@click.option('--id', required=True, type=click.types.STRING, help='hiworks id')
-@click.option('--passwd', required=True, type=click.types.STRING, help='hiworks password')
+@click.option('--id', required=False, type=click.types.STRING, help='hiworks id')
+@click.option('--passwd', required=False, type=click.types.STRING, help='hiworks password')
 def checkout(_id: str, passwd: str):
+    if _id is None or passwd is None:
+        conf = config()
+        _id = conf['default']['id']
+        passwd = conf['default']['password']
+
     browser = Chrome(Log.logger('checkout'))
     browser.checkout(_id, passwd)
 
