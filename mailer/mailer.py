c254a8e2a8d43d75abe6a65f8290ae83c893bea6
sm.yoo <sm.yoo.work@gmail.com>
2022-11-10 08:35:54 +0900

    fix: mailer property name session > smtp

--- mailer/mailer.py
+++ mailer/mailer.py
@@ -1,20 +1,19 @@
 import smtplib
 from email.mime.text import MIMEText
-
 from mailer.abstracts import Mailer
 
 
 class SimpleMailer(Mailer):
 
     def __init__(self, host: str, login_id: str, login_pass: str):
-        self.session = smtplib.SMTP(host, 587)
-        self.session.starttls()
+        self.smtp = smtplib.SMTP(host, 587)
+        self.smtp.starttls()
         self.login_id = login_id
         self.login_pass = login_pass
         self.is_login = False
 
     def login(self, login_id: str, login_pass: str):
-        self.session.login(login_id, login_pass)
+        self.smtp.login(login_id, login_pass)
         self.is_login = True
 
     def send(self, to: str, subject: str, msg: str) -> bool:
@@ -24,8 +23,8 @@ class SimpleMailer(Mailer):
         if not self.is_login:
             self.login(self.login_id, self.login_pass)
         try:
-            self.session.sendmail(self.login_id, to, msg.as_string())
-            self.session.quit()
+            self.smtp.sendmail(self.login_id, to, msg.as_string())
+            self.smtp.quit()
 
             return True
         except:
