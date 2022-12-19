7d50462a435cac66078d007d8b763a0d6c79419d
miniyus <miniyu97@gmail.com>
2022-12-19 22:07:49 +0900

    feat: create command batch file example

--- hiworks-checker.bat
+++ hiworks-checker.bat
@@ -1,4 +1,4 @@
 @echo off
 
 SET BASEDIR=%~dp0
-cd "%BASEDIR%" && "%BASEDIR%venv\Scripts\activate.bat" && python3 "%BASEDIR%cli.py" "%~1"
+cd "%BASEDIR%" && "%BASEDIR%venv\Scripts\activate.bat" && python "%BASEDIR%cli.py" "%~1"
